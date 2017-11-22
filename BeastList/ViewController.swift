b53aa680fda50cc4de60b8860736bce6f4e40b97
Admin <admin@Admins-MacBook-Pro.local>
2017-11-22 11:47:15 -0800

    Beast List

--- BeastList/ViewController.swift
+++ BeastList/ViewController.swift
@@ -8,18 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate{
+    var tasks = [""]
+    @IBAction func beastButtonPressed(_ sender: UIButton) {
+        tasks.append(taskTextField.text!)
+        ableView.reloadData()
+        taskTextField.text = ""
+    }
+    @IBOutlet weak var taskTextField: UITextField!
+    @IBOutlet weak var ableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        ableView.dataSource = self
+        ableView.delegate = self
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+       
     }
 
-
 }
-
+extension ViewController: UITableViewDataSource {
+    func tableView(_ ableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return tasks.count
+    }
+    func tableView(_ ableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = ableView.dequeueReusableCell(withIdentifier: "MyCell", for: indexPath)
+        cell.textLabel?.text = tasks[indexPath.row]
+        return cell
+    }
+    func tableView(_ ableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Section: \(indexPath.section) and Row: \(indexPath.row)")
+        tasks.remove(at: indexPath.row)
+        ableView.reloadData()
+    }
+    
+}
